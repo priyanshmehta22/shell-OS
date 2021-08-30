4ae2059e4707e29688747e630d84bbca8455bf6b
Priyansh Mehta <73767138+priyanshmehta22@users.noreply.github.com>
2021-08-30 09:40:49 +0530

    Create for loop fact.sh

---
+++ for loop fact.sh
@@ -0,0 +1,11 @@
+#factorial using for loop
+echo "Enter a number"
+read n
+fact=1
+for((i=2;i<=n;i++))
+{
+ fact=$((fact * i)) #fact = fact * i
+}
+echo “PRIYANSH MEHTA”
+echo “20BCE2292”
+echo “the factorial of a given number is” $fac
